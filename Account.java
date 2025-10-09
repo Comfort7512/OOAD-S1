f6bc6267433390707239535417b6293658ebdde4
Comfort7512 <cse24-059@thuto.bac.ac.bw>
2025-10-09 17:43:13 +0200

    Delete Account.java

--- Account.java
+++
@@ -1,9 +0,0 @@
-public abstract class Account {
-    String acccountNumber;
-    String branchCode;
-    String accountType;
-    int balance;
-    
-    public abstract int deposit(int amount);
-    public abstract void viewBalance();
-}
