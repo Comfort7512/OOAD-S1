6a24d149aa2b8feddfd7e0bb6a568f75b57d87e7
Comfort7512 <cse24-059@thuto.bac.ac.bw>
2025-10-09 17:44:30 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test{
-    public static void main(String args[]){
-
-        SavingsAccount account1 = new SavingsAccount();
-        account1.deposit(100);
-        account1.viewBalance();
-    }
-}
