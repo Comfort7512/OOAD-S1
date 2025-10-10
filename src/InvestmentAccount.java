3bdeec950168b81e6f32ab08475735f4c2c78f76
Henry Comfort Mothobi <mothobihenry71@gmail.com>
2025-10-10 08:12:37 +0200

    Updated Interest to apply every 30 days

--- src/InvestmentAccount.java
+++ src/InvestmentAccount.java
@@ -40,15 +40,17 @@ public class InvestmentAccount extends Account implements PayInterest,Withdraw {
     public String payInterest() {
 
         Timer time = new Timer();
+        long period = 30L * 24 * 60 * 60 * 1000; // every 30 days interest will apply
 
         TimerTask task = new TimerTask() {
+
             @Override
             public void run() {
                 balance += balance*interestRate;
                 System.out.println("Interest applied, new balance is "+balance);
             }
         };
-        time.scheduleAtFixedRate(task,0,5000);
+        time.scheduleAtFixedRate(task,0,period);
 
         return "Automatic interest application started. Interest will apply every 5 seconds.";
     }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -22,6 +22,8 @@ public class SavingsAccount extends Account implements PayInterest {
     public String payInterest() {
 
         Timer time = new Timer();
+        long period = 30L * 24 * 60 * 60 * 1000; // every 30 days interest will apply
+
 
         TimerTask task = new TimerTask() {
             @Override
@@ -30,7 +32,7 @@ public class SavingsAccount extends Account implements PayInterest {
                 System.out.println("Interest applied, new balance is "+balance);
             }
         };
-        time.scheduleAtFixedRate(task,0,5000);
+        time.scheduleAtFixedRate(task,0,period);
 
         return "Automatic interest application started. Interest will apply every 5 seconds.";
     }
